b9d63e3de0c199e1570c2a10a5cf95e1a2df205e
Qhristen <odeyxtian@gmail.com>
2020-09-11 18:48:41 +0100

    Updates

---
+++ precache-manifest.d04d8276fc0eaea68ab7c30e549191b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881f2d6c512eeac97325cea065da9297",
+    "url": "/ega/index.html"
+  },
+  {
+    "revision": "3a08482346311d90b9a6",
+    "url": "/ega/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "767e9dbf1c1ddf85e164",
+    "url": "/ega/static/css/main.a5a605e3.chunk.css"
+  },
+  {
+    "revision": "3a08482346311d90b9a6",
+    "url": "/ega/static/js/2.7b0f5412.chunk.js"
+  },
+  {
+    "revision": "767e9dbf1c1ddf85e164",
+    "url": "/ega/static/js/main.94e0d8d3.chunk.js"
+  },
+  {
+    "revision": "90ce95afba19badcafdf",
+    "url": "/ega/static/js/runtime-main.2eabb3a0.js"
+  },
+  {
+    "revision": "0d2c9a48d0ee14c1bbf427507fa9fae0",
+    "url": "/ega/static/media/feedback.0d2c9a48.PNG"
+  },
+  {
+    "revision": "2e7392bd69172cb64fb648d6a5abb830",
+    "url": "/ega/static/media/giphy.2e7392bd.gif"
+  },
+  {
+    "revision": "46d1d88b76dc523cd9aa87cd135c0814",
+    "url": "/ega/static/media/logo.46d1d88b.svg"
+  },
+  {
+    "revision": "31ce1c5d31205e0667a2092e81d8ff8e",
+    "url": "/ega/static/media/smashit.31ce1c5d.jpg"
+  },
+  {
+    "revision": "0400e37ceca4bb271a47307b995749c0",
+    "url": "/ega/static/media/textwrite.0400e37c.jpg"
+  },
+  {
+    "revision": "810e69e21cbf4d75a39bf7d51c9ade70",
+    "url": "/ega/static/media/zeemap.810e69e2.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ega/precache-manifest.d04d8276fc0eaea68ab7c30e549191b4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ega/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
